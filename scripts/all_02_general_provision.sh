9d3b2c0bb69a71c6a2586fe24d07ddad41beef3b
Adrian Arba <adrian.arba@outlook.com>
2018-11-23 12:11:37 +0200

    removed domain refferences

--- scripts/all_02_general_provision.sh
+++ scripts/all_02_general_provision.sh
@@ -12,7 +12,7 @@ wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum install -y ./epel-release-latest-*.noarch.rpm
 rm -rf epel-release-latest-7.noarch.rpm
 echo "10.124.55.231    857222-Nexus1-build.tjxeurope.com    nexus.tjxeurope.com" >> /etc/hosts
-echo -e "$KUBERNETES_MASTER_IP kube-master.kubernetes
-10.0.21.41 kube-work01.kubernetes
-10.0.21.42 kube-work02.kubernetes" >> /etc/hosts
+echo -e "$KUBERNETES_MASTER_IP kube-master
+10.0.21.41 kube-work01
+10.0.21.42 kube-work02" >> /etc/hosts
 rpm -ivh https://centos7.iuscommunity.org/ius-release.rpm
