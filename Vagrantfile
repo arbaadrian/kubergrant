4362a6e43e4793a700f5760e0dc1fbce858d0112
Adrian Arba <aarba@deloittece.com>
2021-11-05 18:47:14 +0200

    Removed a lot of old deprecated configs and added Cheeses

--- Vagrantfile
+++ Vagrantfile
@@ -23,14 +23,7 @@ Vagrant.configure("2") do |config|
 
     ## 31557 is the port for Dashboard
     controlplane.vm.network :forwarded_port, guest: x.fetch('ports').fetch('dashboard_port'), host: x.fetch('ports').fetch('dashboard_port')
-    ## 31558 is the port for Kibana
-    controlplane.vm.network :forwarded_port, guest: 5601, host: 31558
 
-    ## 31559 is the port for Prometheus server if we move it to port 81
-    controlplane.vm.network :forwarded_port, guest: 81, host: 31559
-
-    ## this port might change - 32185 for wordpress
-    controlplane.vm.network :forwarded_port, guest: x.fetch('ports').fetch('wordpress_port'), host: x.fetch('ports').fetch('wordpress_port')
     controlplane.vm.provider :virtualbox do |vb|
       # vb.name = "controlplane"+"."+x.fetch('domain')
       vb.name = "controlplane"
@@ -50,32 +43,14 @@ Vagrant.configure("2") do |config|
     controlplane.vm.provision "shell", path: "scripts/all_02_general_provision.sh"
     controlplane.vm.provision "shell", path: "scripts/all_03_kubernetes_prep.sh"
     controlplane.vm.provision :unix_reboot
-    controlplane.vm.provision "file", source: "files/kube-flannel.yml", destination: "/tmp/kube-flannel.yml"
     controlplane.vm.provision "shell", path: "scripts/controlplane_01_kubernetes_cluster.sh"
     controlplane.vm.provision "shell", path: "scripts/all_04_cleanup.sh"
-    # Files below will be used to set up Dashboard together with the instructions in zero_others_kubernetes_stuff.sh
-    controlplane.vm.provision "file", source: "files/kubernetes-dashboard.yml", destination: "/tmp/kubernetes-dashboard.yml"
-    controlplane.vm.provision "file", source: "files/dashboard-adminuser.yml", destination: "/tmp/dashboard-adminuser.yml"
-    controlplane.vm.provision "file", source: "files/dashboard-adminuser-rbac.yml", destination: "/tmp/dashboard-adminuser-rbac.yml"
-    # Files below will be used to set up ELK together with the instructions in zero_others_kubernetes_stuff.sh
-    controlplane.vm.provision "file", source: "files/persistent-volume-elk-01.yml", destination: "/tmp/persistent-volume-elk-01.yml"
-    controlplane.vm.provision "file", source: "files/persistent-volume-elk-02.yml", destination: "/tmp/persistent-volume-elk-02.yml"
-    controlplane.vm.provision "file", source: "files/elk_01_k8s_global.tar.gz", destination: "/tmp/elk_01_k8s_global.tar.gz"
-    controlplane.vm.provision "file", source: "files/elk_02_elasticsearch.tar.gz", destination: "/tmp/elk_02_elasticsearch.tar.gz"
-    controlplane.vm.provision "file", source: "files/elk_03_kibana.tar.gz", destination: "/tmp/elk_03_kibana.tar.gz"
-    controlplane.vm.provision "file", source: "files/elk_04_beats_init.tar.gz", destination: "/tmp/elk_04_beats_init.tar.gz"
-    controlplane.vm.provision "file", source: "files/elk_05_beats_agents.tar.gz", destination: "/tmp/elk_05_beats_agents.tar.gz"
-    # Files below will be used to set up Ceph
-    controlplane.vm.provision "file", source: "files/rook_ceph_grafanaingress.tar.gz", destination: "/tmp/rook_ceph_grafanaingress.tar.gz"
-    controlplane.vm.provision "file", source: "files/wordpress.tar.gz", destination: "/tmp/wordpress.tar.gz"
-    # zero_others_kubernetes_stuff.sh
-    controlplane.vm.provision "file", source: "scripts/zero_others_kubernetes_stuff.sh", destination: "/tmp/zero_others_kubernetes_stuff.sh"
     # controlplane.vm.synced_folder "files/", "/tmp/files"
     # controlplane.vm.synced_folder "scripts/", "/tmp/scripts"
 
   end
 
-  node_ip = IPAddr.new(x.fetch('ip').fetch('node'))
+  node_ip = IPAddr.new(x.fetch('ip').fetch('node_ip_range_from'))
   (1..x.fetch('node').fetch('count')).each do |i|
     c = x.fetch('node')
     hostname = "worker%02d" % i
@@ -106,8 +81,8 @@ Vagrant.configure("2") do |config|
       worker.vm.provision :unix_reboot
       worker.vm.provision "shell", path: "scripts/worker_01_kubernetes_cluster_add.sh"
       worker.vm.provision "shell", path: "scripts/all_04_cleanup.sh"
-      # worker.vm.synced_folder "files/", "/tmp/files"
-      # worker.vm.synced_folder "scripts/", "/tmp/scripts"
+      worker.vm.synced_folder "files/", "/tmp/files"
+      worker.vm.synced_folder "scripts/", "/tmp/scripts"
     end
   end
 end
--- scripts/zero_others_kubernetes_stuff.sh
+++ scripts/zero_others_kubernetes_stuff.sh
@@ -4,41 +4,39 @@
 ## INGRESS ##
 #############
 
-kubectl create namespace ingress
-kubectl create -f /tmp/default-backend-deployment.yaml -n ingress
-kubectl create -f /tmp/default-backend-service.yaml -n ingress
-kubectl create -f /tmp/nginx-ingress-controller-config-map.yaml -n ingress
-kubectl create -f /tmp/nginx-ingress-controller-roles.yaml -n ingress
-kubectl create -f /tmp/nginx-ingress-controller-deployment.yaml -n ingress
-kubectl create -f /tmp/nginx-ingress.yaml -n ingress
-kubectl create -f /tmp/nginx-ingress-controller-service.yaml -n ingress
+kubectl create namespace ingress-nginx
+helm install ingress-nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --set rbac.create=true
 
+# we have to edit he validation webhook config to allow creation of ingresses for all resources otherwise ingress creations will fail
+kubectl get ValidatingWebhookConfiguration ingress-nginx-admission -oyaml > ingress-nginx-admission.yaml
+sed -i 's/networking.k8s.io/\"\"/' ingress-nginx-admission.yaml
+kubectl apply -f ingress-nginx-admission.yaml
+
+###############
+### CHEESES ###
+###############
+
+kubectl apply -f /vagrant/files/cheeses/
 
 ###############
 ## DASHBOARD ##
 ###############
 
 ## METHOD 1
-kubectl apply -f /tmp/kubernetes-dashboard.yml
+kubectl apply -f /vagrant/files/dashboard/kubernetes-dashboard.yml
 kubectl get service kubernetes-dashboard -n kube-system
 ### The following line will add the dashboard to the cluster info
 kubectl label services kubernetes-dashboard kubernetes.io/cluster-service=true -n kube-system
 
-# Manual
-# kubectl -n kube-system edit service kubernetes-dashboard
-# kubectl -n kube-system get service kubernetes-dashboard
-
 # Create user for Dashboard admin access
-kubectl apply -f /tmp/dashboard-adminuser.yml
-kubectl apply -f /tmp/dashboard-adminuser-rbac.yml
+kubectl apply -f /vagrant/files/dashboard/dashboard-adminuser.yml
+kubectl apply -f /vagrant/files/dashboard/dashboard-adminuser-rbac.yml
 
 # Print access token for said user
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
 
 # Access below URL with Mozilla and paste in the token (will not work with Chrome)
-https://controlplane:31557
-
-
+# https://controlplane:31557
 
 ## METHOD 2
 ## Deploy Dashboard - reference: https://github.com/kubernetes/dashboard/wiki/Creating-sample-user
@@ -72,28 +70,6 @@ https://controlplane:31557
 
 #######################################################################################################################################
 #######################################################################################################################################
-
-#########
-## ELK ##
-#########
-
-cd /tmp
-kubectl apply -f persistent-volume-elk-01.yml
-kubectl apply -f persistent-volume-elk-02.yml
-tar -zxvf elk_01_k8s_global.tar.gz
-tar -zxvf elk_02_elasticsearch.tar.gz
-tar -zxvf elk_03_kibana.tar.gz
-tar -zxvf elk_04_beats_init.tar.gz
-tar -zxvf elk_05_beats_agents.tar.gz
-kubectl apply -f elk_01_k8s_global
-kubectl apply -f elk_02_elasticsearch
-kubectl apply -f elk_03_kibana
-kubectl apply -f elk_04_beats_init
-kubectl apply -f elk_05_beats_agents
-git clone https://github.com/kubernetes/kube-state-metrics.git
-kubectl apply -f kube-state-metrics/kubernetes
-
-http://controlplane:31558
 
 #################
 ## Other stuff ##
@@ -137,202 +113,32 @@ http://controlplane:31558
 # kubectl create -f /tmp/ceph-dashboard-nodeip.yml
 
   # to get the password for the ceph 'admin' user
-echo -e "export CEPH_DASHBOARD=$(kubectl -n rook-ceph get secret rook-ceph-dashboard-password -o jsonpath="{['data']['password']}" | base64 --decode && echo)" >> /tmp/vars
-source /tmp/vars
-echo $CEPH_DASHBOARD
+# echo -e "export CEPH_DASHBOARD=$(kubectl -n rook-ceph get secret rook-ceph-dashboard-password -o jsonpath="{['data']['password']}" | base64 --decode && echo)" >> /tmp/vars
+# source /tmp/vars
+# echo $CEPH_DASHBOARD
 
   ### Print cluster info on screen
 kubectl cluster-info
-
-  ## Start an ingress and add an application
-helm install --name apps-ingresses stable/nginx-ingress --set rbac.create=true
-  # here, we have to expose the port that is opened in vagrant to a port inside kubernetes (example for Dashboard)
-# kubectl edit svc apps-ingresses-nginx-ingress-controller
-# (...)
-# spec
-#   ports:
-#   - name: http-mgmt
-#     nodePort: 31557
-#     port: 18080
-#     protocol: TCP
-#     targetPort: 18080
-# (...)
-
-kubectl edit deployments apps-ingresses-nginx-ingress-controller
-  # here, we have to add the port that the application will live at
-(...)
-spec:
-  template:
-    spec:
-      containers:
-        ports:
-        - containerPort: 18080
-          protocol: TCP
-(...)
-
-  # now we will add an ingress for nginx, it will return a few nginx details when accessed like so http://controlplane:31557/nginx_status (or IP)
-cat > /tmp/nginx-ingress.yaml <<EOF
----
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: nginx-ingress
-spec:
-  rules:
-  - host: controlplane
-    http:
-      paths:
-      - backend:
-          serviceName: nginx-ingress
-          servicePort: 18080
-        path: /nginx_status
-EOF
-kubectl create -f /tmp/nginx-ingress.yaml
-
-  # for apps - none yet
-cat > /tmp/app-ingress.yaml <<EOF
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  annotations:
-    nginx.ingress.kubernetes.io/rewrite-target: /
-  name: app-ingress
-spec:
-  rules:
-  - host: controlplane
-    http:
-      paths:
-      - backend:
-          serviceName: appsvc1
-          servicePort: 80
-        path: /app1
-      - backend:
-          serviceName: appsvc2
-          servicePort: 80
-        path: /app2
-EOF
-
-kubectl create -f /tmp/app-ingress.yaml
 
 ################
 ## ROOK, CEPH ##
 ################
 
-cd /tmp
-git clone https://github.com/rook/rook.git
-kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/operator.yaml
-kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/cluster.yaml
-kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/storageclass.yaml
-  # this is a patch for storageclass
-kubectl patch storageclass rook-ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
-kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/toolbox.yaml
-  # check if toolbox pod is up
-kubectl -n rook-ceph get pod -l "app=rook-ceph-tools"
-  # get into toolbox pod and run commands to see the ceph cluster info
-kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}') bash
-    ceph status
-    ceph osd status
-    ceph df
-    rados df
-  # to remove toolbox: kubectl -n rook-ceph delete deployment rook-ceph-tools
-sudo yum install -y xfsprogs
-kubectl edit service apps-ingresses-nginx-ingress-controller
-  # check to which port is port 80 receiving ingress access - 31096? kubectl get services
-(...)
-spec
-  - name: http
-    nodePort: 31096
-    port: 80
-    protocol: TCP
-    targetPort: http
-(...)
-
-##########################
-## GRAFANA & PROMETHEUS ##
-##########################
-
-helm init
-helm repo update
-helm install --name prometheus stable/prometheus
-helm install --name grafana stable/grafana
-
-cat > /tmp/grafana-ingress.yaml <<EOF
----
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  name: grafana-ingress
-spec:
-  rules:
-  - host: controlplane
-    http:
-      paths:
-      - backend:
-          serviceName: grafana
-          servicePort: 80
-        path: /
-EOF
-
-kubectl create -f /tmp/grafana-ingress.yaml
-  # add port for prometheus-server in the ingress controller
-kubectl edit service apps-ingresses-nginx-ingress-controller
-(...)
-spec
-  - name: prometheus-server
-    nodePort: 31559
-    port: 81
-    protocol: TCP
-    targetPort: http
-(...)
-
-kubectl edit service prometheus-server
-  # change prometheus-server service to listen on port 81
-(...)
-spec:
-  ports:
-  - name: http
-    port: 81
-    protocol: TCP
-    targetPort: 9090
-(...)
-
-  # get admin password for grafana
-kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
-
-  # access
-http://controlplane:31096
-
-  # below command will give the IP of the prometheus endpoint that needs to be added into grafana datasources
-kubectl describe service prometheus-server | grep -i endpoints
-
-#########################################
-## WORDPRESS & MYSQL (on top of above) ##
-#########################################
-
-kubectl create -f rook/cluster/examples/kubernetes/mysql.yaml
-  # edit wordpress.yaml in the Deployment section, make sure version apps/v1beta1 is set instead of apps/v1
-kubectl create -f rook/cluster/examples/kubernetes/wordpress.yaml
-
-# cat > /tmp/wordpress-ingress.yaml <<EOF
-# ---
-# apiVersion: networking.k8s.io/v1
-# kind: Ingress
-# metadata:
-#   name: wordpress-ingress
-# spec:
-#   rules:
-#   - host: controlplane
-#     http:
-#       paths:
-#       - backend:
-#           serviceName: wordpress
-#           servicePort: 80
-#         path: /wp-admin
-# EOF
-
-# kubectl create -f /tmp/wordpress-ingress.yaml
-
-  # kubectl get services and open up in vagrant the port assigned for wordpress, ie 32185, and do vagrant reload controlplane
-  # do something on wordpress http://controlplane:31096/wp-admin/upload.php
-  # if we delete the mysql pod, it will still come back around with the same content
-kubectl delete pod $(kubectl get pod -l app=wordpress,tier=mysql -o jsonpath='{.items[0].metadata.name}')
+# cd /tmp
+# git clone https://github.com/rook/rook.git
+# kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/operator.yaml
+# kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/cluster.yaml
+# kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/storageclass.yaml
+#   # this is a patch for storageclass
+# kubectl patch storageclass rook-ceph-block -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+# kubectl create -f /tmp/rook/cluster/examples/kubernetes/ceph/toolbox.yaml
+#   # check if toolbox pod is up
+# kubectl -n rook-ceph get pod -l "app=rook-ceph-tools"
+#   # get into toolbox pod and run commands to see the ceph cluster info
+# kubectl -n rook-ceph exec -it $(kubectl -n rook-ceph get pod -l "app=rook-ceph-tools" -o jsonpath='{.items[0].metadata.name}') bash
+#     ceph status
+#     ceph osd status
+#     ceph df
+#     rados df
+#   # to remove toolbox: kubectl -n rook-ceph delete deployment rook-ceph-tools
+# sudo yum install -y xfsprogs
